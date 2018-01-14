6575e2271cff72e549b8b325f728ac5aa508437c
Arpan Bhalla <arpanbhalla@gmail.com>
2018-01-14 17:30:22 +1100

    fixed user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user creation' do
+    user = User.create(email: 'abc@example.com', first_name: 'John',
+                       last_name: 'Doe', password: 'chicken',
+                       password_confirmation: 'chicken')
+    context 'with valid attributes' do
+      it 'can be created with valid params' do
+        expect(user).to be_valid
+      end
+
+      it 'cannot be created with email, firstname & lastname' do
+        user.first_name = nil
+        user.last_name = nil
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+    end
+  end
 end
