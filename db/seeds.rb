13fa947f6eceaca5fedf1ebfd1a37156042217e5
Arpan Bhalla <arpanbhalla@gmail.com>
2018-01-23 19:53:50 +1100

    fixed tests and updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-@user = User.create(email: 'abc@example.com', first_name: 'John',
-                   last_name: 'Doe', password: 'chicken',
-                   password_confirmation: 'chicken')
+@user = User.create(email: 'arpanbhalla@gmail.com', first_name: 'Arpan',
+                   last_name: 'Bhalla', password: 'chicken',
+                   password_confirmation: 'chicken', type: 'AdminUser')
 
 100.times do |post|
   Post.create(date: Faker::Date.backward(100),
